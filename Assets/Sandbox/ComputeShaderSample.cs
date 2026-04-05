9d3fa6462ba9b2f23fdd5d94870ab354875f2ee7
amenonegames <100146683+amenonegames@users.noreply.github.com>
2026-04-05 20:19:55 +0900

    feat(sandbox): add compute shader sample

---
+++ Assets/Sandbox/ComputeShaderSample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Sandbox
+{
+    public class ComputeShaderSample :MonoBehaviour
+    {
+        private static readonly int Result = Shader.PropertyToID("Result");
+        private static readonly int Texture1 = Shader.PropertyToID("Texture");
+        private static readonly int Length = Shader.PropertyToID("Length");
+        private static readonly int Width = Shader.PropertyToID("pixelPerDivW");
+        private static readonly int Height = Shader.PropertyToID("pixelPerDivH");
+        [SerializeField] private ComputeShader shader;
+        [SerializeField] private Texture texture;
+        private readonly int _divCount = 10;
+
+        private void Start()
+        {
+            int pixelPerDivW = texture.width / _divCount; // 分割区画ごとのピクセル数を算出
+            pixelPerDivW = pixelPerDivW - (1 - pixelPerDivW % 2); // 奇数に切り下げ
+            int pixelPerDivH = texture.height / _divCount;
+            pixelPerDivH = pixelPerDivH -  (1 - pixelPerDivH % 2); // 奇数に切り下げ
+
+            int num = _divCount * _divCount;
+            ComputeBuffer buffer = new ComputeBuffer(num, sizeof(float));
+
+            int kernelID = shader.FindKernel("CSMain");
+
+            shader.SetBuffer(kernelID , Result , buffer);
+            shader.SetTexture(kernelID,Texture1,texture);
+           shader.SetInt(Length,_divCount);
+           shader.SetInt(Width,pixelPerDivW);
+           shader.SetInt(Height,pixelPerDivH);
+
+           float[] rawData = new float[num];
+
+            shader.Dispatch(kernelID, _divCount, _divCount, 1);
+            buffer.GetData(rawData);
+            buffer.Release();
+        }
+    }
+}
