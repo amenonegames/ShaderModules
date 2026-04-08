8ea14032e1bf2764126eb96a2cef3c866791d1a9
amenonegames <100146683+amenonegames@users.noreply.github.com>
2026-04-08 14:01:20 +0900

    refactor(sandbox): update particle kernel to modern unity apis

--- Assets/Sandbox/ParticleKernelTest.cs
+++ Assets/Sandbox/ParticleKernelTest.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Random = UnityEngine.Random;
 
 namespace Sandbox
 {
-
     public class ParticleKernelTest : MonoBehaviour
     {
         private static readonly int ParticleBuffer = Shader.PropertyToID("_ParticleBuffer");
         private static readonly int DeltaTime = Shader.PropertyToID("_DeltaTime");
+
         [SerializeField] private int _count = 10000;
         [SerializeField] private ComputeShader _computeShader;
         [SerializeField] private Mesh _particleMesh;
         [SerializeField] private MeshFilter _targetMeshFilter;
         [SerializeField] private Material _particleMat;
         [SerializeField] private Color _color = Color.blue;
-        
-        private ComputeBuffer _particleBuffer;
-        private ComputeBuffer _argBuffer;
-        private uint[] _args = new uint [5]{0,0,0,0,0};
-        private int _kernelId = 0;
-        
+
+        private GraphicsBuffer _particleBuffer;
+        private GraphicsBuffer _argBuffer;
+        private int _kernelId;
+        private RenderParams _renderParams;
+
         public struct Particle
         {
             public Vector3 basePosition;
@@ -34,13 +34,14 @@ namespace Sandbox
         private void Start()
         {
             _kernelId = _computeShader.FindKernel("ParticleMain");
-            List<Vector3> vertices = new List<Vector3>();
-            _targetMeshFilter.mesh.GetVertices(vertices);
-            Particle[] particles = new Particle[_count];
 
+            var vertices = new List<Vector3>();
+            _targetMeshFilter.mesh.GetVertices(vertices);
+
+            var particles = new Particle[_count];
             for (int i = 0; i < _count; i++)
             {
-                particles[i] = new Particle()
+                particles[i] = new Particle
                 {
                     basePosition = vertices[i % vertices.Count],
                     position = vertices[i % vertices.Count] + Random.insideUnitSphere * 10f,
@@ -48,19 +49,28 @@ namespace Sandbox
                     scale = Random.Range(0.01f, 0.02f),
                 };
             }
-            _particleBuffer = new ComputeBuffer(_count, Marshal.SizeOf(typeof(Particle)));
+
+            _particleBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _count, Marshal.SizeOf<Particle>());
             _particleBuffer.SetData(particles);
             _computeShader.SetBuffer(_kernelId, ParticleBuffer, _particleBuffer);
             _particleMat.SetBuffer(ParticleBuffer, _particleBuffer);
 
-            int subMeshIndex = 0;
-            _args[0] = _particleMesh.GetIndexCount(subMeshIndex);
-            _args[1] = (uint)_count;
-            _args[2] = _particleMesh.GetIndexStart(subMeshIndex);
-            _args[3] = _particleMesh.GetBaseVertex(subMeshIndex);
+            var args = new uint[5]
+            {
+                _particleMesh.GetIndexCount(0),
+                (uint)_count,
+                _particleMesh.GetIndexStart(0),
+                _particleMesh.GetBaseVertex(0),
+                0,
+            };
+            _argBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, 1, sizeof(uint) * args.Length);
+            _argBuffer.SetData(args);
 
-            _argBuffer = new ComputeBuffer(1, sizeof(uint) * _args.Length, ComputeBufferType.IndirectArguments);
-            _argBuffer.SetData(_args);
+            _renderParams = new RenderParams(_particleMat)
+            {
+                worldBounds = new Bounds(Vector3.zero, Vector3.one * 32f),
+                matProps = new MaterialPropertyBlock(),
+            };
         }
 
         private void Update()
@@ -68,13 +78,13 @@ namespace Sandbox
             _computeShader.SetFloat(DeltaTime, Time.deltaTime);
             _computeShader.Dispatch(_kernelId, _count / 8, 1, 1);
 
-            Graphics.DrawMeshInstancedIndirect(_particleMesh,0,_particleMat,new Bounds(Vector3.zero,Vector3.one * 32f),_argBuffer);
+            Graphics.RenderMeshIndirect(_renderParams, _particleMesh, _argBuffer);
         }
 
         private void OnDestroy()
         {
-            _particleBuffer?.Release();
-            _argBuffer?.Release();
+            _particleBuffer?.Dispose();
+            _argBuffer?.Dispose();
         }
     }
 }
